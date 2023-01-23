11a0503b75601518b0c3f185c88bc4f4c53e377d
Molly Adamsson <adamssonmolly@gmail.com>
2023-01-23 11:27:51 +0000

    Add option for multiple player, also letters and num for rows and columns in grid

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 from random import randint
+
+player_ships = [['']*8 for x in range(8)]
+player_guesses = [['']*8 for x in range(8)]
+computer_ships = [['']*8 for x in range(8)]
+computer_guesses = [['']*8 for x in range(8)]
+
+let_to_num ={'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
